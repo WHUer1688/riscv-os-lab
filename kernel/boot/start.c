abd3b5cf51a886c59e6349b78fc58b784610c680
cxy <cxy66726@gmail.com>
2025-09-18 20:27:10 +0800

    Lab-1 wakeup

--- kernel/boot/start.c
+++ kernel/boot/start.c
@@ -4,6 +4,17 @@ __attribute__ ((aligned (16))) uint8 CPU_stack[4096 * NCPU];
 
 void main();
 
+// CLINT (Core Local Interruptor) 基地址
+#define CLINT_BASE 0x2000000L
+#define CLINT_MSIP(hartid) (CLINT_BASE + 4*(hartid))
+
+// 唤醒指定的hart
+void wakeup_hart(int hartid)
+{
+    // 设置MSIP位来唤醒hart
+    *(volatile uint32*)CLINT_MSIP(hartid) = 1;
+}
+
 void start()
 {
     // 设置M模式下的特权级别为Supervisor模式，用于mret返回
@@ -30,6 +41,13 @@ void start()
     // 将当前CPU的hartid保存在tp寄存器中
     int id = r_mhartid();
     w_tp(id);
+
+    // 如果是hart0，唤醒其他hart
+    if(id == 0) {
+        // 唤醒hart1和hart2
+        wakeup_hart(1);
+        wakeup_hart(2);
+    }
 
     // 切换到Supervisor模式并跳转到main函数
     asm volatile("mret");
